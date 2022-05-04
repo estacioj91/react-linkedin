4c3f8b2165cc6a706b055ccc52dec748486d2f5e
John Estacio <john.estacio@workato.com>
2022-05-04 14:48:25 -0700

    header start

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>Header</h2>
+            <div className="header__left">
+                <img src="" alt=""/>
+                <div className="header__search">
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
