69d74db51083dc797a5aaaf1a782fd4cdc19c8c8
John Estacio <john.estacio@workato.com>
2022-05-04 14:59:55 -0700

    logo added

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import logo from './assets/linkedin.png';
 function Header() {
     return (
         <div className="header">
             <h2>Header</h2>
             <div className="header__left">
-                <img src="" alt=""/>
+                <img src={logo} alt=""/>
                 <div className="header__search">
+                    <SearchIcon/>
                     <input type="text"/>
                 </div>
             </div>
