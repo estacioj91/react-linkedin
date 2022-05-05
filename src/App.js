b48807ab9435ccac0b36433c4505b66a395f6299
John Estacio <john.estacio@workato.com>
2022-05-04 23:59:12 -0700

    working nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './app.css';
+import './App.css';
 import Header from './Header.js';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a Linkedin Clone</h1>
       <Header />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption.js'
 import logo from './assets/linkedin.png';
+import './Header.css'
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+
 function Header() {
+    const headShot = "https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg";
     return (
         <div className="header">
-            <h2>Header</h2>
             <div className="header__left">
-                <img src={logo} alt=""/>
+                <img src={logo} alt="linkedin logo"/>
                 <div className="header__search">
                     <SearchIcon/>
                     <input type="text"/>
                 </div>
             </div>
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home"></HeaderOption>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"></HeaderOption>
+                <HeaderOption Icon={BusinessCenterIcon} title="My Network"></HeaderOption>
+                <HeaderOption Icon={ChatIcon} title="My Network"></HeaderOption>
+                <HeaderOption Icon={NotificationsIcon} title="My Network"></HeaderOption>
+                <HeaderOption avatar={headShot} title="me"></HeaderOption>
             </div>
         </div>
     )
