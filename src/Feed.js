95a3c6394d9da3240fccd7528e32916de4ac3196
John Estacio <john.estacio@workato.com>
2022-05-10 00:26:20 -0700

    firebase tst

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState, useId } from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption.js'
@@ -7,33 +7,64 @@ import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import Post from './Post';
-const Feed = () => {
-    const[posts, setPosts] = useState([]);
+import { db } from './firebase.js';
+import { collection, getDocs, addDoc} from "firebase/firestore";
 
-    const sendPost = e => {
-        e.preventDefault();
+const Feed = () => {
+    const [posts, setPosts] = useState([]);
+    const [input, setInput] = useState("")
+    useEffect(() => {
+        getPosts();
+    }, [])
+
+    async function getPosts() {
+        console.log("running get posts")
+        const querySnapshot = await getDocs(collection(db, "asdfas"));
+        querySnapshot.forEach((doc) => {
+            setPosts([...posts, doc.data()]);
+            console.log(doc.data(), posts)
+        })
+        console.log(querySnapshot);
     }
-    return(
+
+    async function sendPost(e) {
+        e.preventDefault();
+        const rand = Math.floor(Math.random() * 100000);
+        console.log("sub", rand)
+
+        try {
+            const docRef = await addDoc(collection(db, "asdfas"), {
+                name: "John estacio",
+                description: "test",
+                message: input,
+                id: rand
+            });
+            console.log("Document written with ID: ", docRef.id)
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+    return (
         <div className="feed">
             <div className="feed__inputContainer">
                 <div className="feed__input">
-                    <CreateIcon/>
+                    <CreateIcon />
                     <form action="">
-                        <input type="text" placeholder="Start a post"/>
+                        <input type="text" placeholder="Start a post" value={input} onChange={e => setInput(e.target.value)} />
                         <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    <InputOption Icon={ImageIcon} title="photo" color="#70b5f9"/>
-                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e"/>
-                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0cbcd"/>
-                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7fc15e"/>
+                    <InputOption Icon={ImageIcon} title="photo" color="#70b5f9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0cbcd" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7fc15e" />
                 </div>
             </div>
-            {posts.map((post) => {
-                return <Post/>
+            {posts.map((post) => {console.log("posts", posts)
+                return <Post key={post.id} name={post.name} message={post.message} description={post.description}/>
             })}
-            <Post name="John Estacio" description="test post" message="this is a test post"/>
+            {/* <Post name="John Estacio" description="test post" message="this is a test post" /> */}
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtZsZGmkA0tL0tB29os2c-exqGXoGiImY",
+    authDomain: "linkedin-64369.firebaseapp.com",
+    projectId: "linkedin-64369",
+    storageBucket: "linkedin-64369.appspot.com",
+    messagingSenderId: "569013277500",
+    appId: "1:569013277500:web:9c917483ee9c70f5460439"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+
+export {db, auth};
