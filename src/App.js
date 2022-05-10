3b60e3bd2295020276423958cf48965e5cff8c42
John Estacio <john.estacio@workato.com>
2022-05-10 15:20:19 -0700

    redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Header from './Header.js';
 import Sidebar from './Sidebar.js'
 import Feed from './Feed.js'
+import Login from './Login.js'
+import {useSelector} from 'react-redux';
+import {selectUser} from './features/userSlice.js';
+
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
+      {user ? <Login/> : (
+        <div className="app__body">
         <Sidebar/>
         <Feed/>
       </div>
+      )}
+      
     </div>
   );
 }
